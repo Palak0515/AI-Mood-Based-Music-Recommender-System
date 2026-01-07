fc76caa4b163f7d918526c9fecd0b46a65861ea2
Palak Sharma <palaksharma0515@gmail.com>
2026-01-07 12:51:25 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -26,8 +26,7 @@ document.getElementById("detectBtn").addEventListener("click", async () => {
 
     try {
         // Send image to backend
-        debugger;
-        const res = await fetch("http://127.0.0.1:5000/songs", {
+        const res = await fetch("/songs", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ image: base64Image })
@@ -62,3 +61,4 @@ document.getElementById("detectBtn").addEventListener("click", async () => {
         console.error(err);
     }
 });
+
